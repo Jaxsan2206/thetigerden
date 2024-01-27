c45321cc9f9e8898610a17dd45eed6347ecee8b4
Jaxsan Sivanesan <jaxsan.sivanesan@gmail.com>
2024-01-27 20:01:55 +0000

    Fixed text block alignment issues

--- libs/components/src/lib/TextBlock/TextBlock.tsx
+++ libs/components/src/lib/TextBlock/TextBlock.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Container } from '../Layout/Layout'
 import { ITextBlockProps } from './TextBlock.interface'
+import { Box } from '../FlexBox/FlexBox'
 
 const TextBlock: React.FC<ITextBlockProps> = ({ elements = null }) => {
   return (
-    <Container>
-        {elements}
-    </Container>
-  )
+    <Box paddingY={"large"}>
+      <Container>{elements}</Container>
+    </Box>
+  );
 }
 
 export default TextBlock
